5489c7fce07032e54b79477d6ee71bffb1cc9fc8
Vanja Maric <vm222hx@student.lnu.se>
2023-09-26 07:50:57 +0200

    Update README.md

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # Nonogram-test-app
+Det här är en test app för [nonogram-generator module](https://github.com/Vanja-Maric/Nonogram-generator/tree/main)
+## 
+
+## Du kan ladda ner .jar file av nonogram-generator module här:
+[jar](https://github.com/Vanja-Maric/Nonogram-generator/blob/main/nonogramGenerator.jar)
--- src/NonogramTableInJFrame.java
+++ src/NonogramTableInJFrame.java
@@ -11,7 +11,7 @@ public class NonogramTableInJFrame {
 
   public JFrame getNonogram(String[][] nonogramGrid, boolean isBlackAndWhite) {
     JFrame appFrame = createNonogramFrame();
-    Box nonogramCellsGrid = createNonogramCellsGrid(nonogramGrid, isBlackAndWhite);
+    Box nonogramCellsGrid = createNonogramCellsGridAndCounts(nonogramGrid, isBlackAndWhite);
     appFrame.add(nonogramCellsGrid);
     appFrame.setVisible(true);
     return appFrame;
@@ -24,41 +24,53 @@ public class NonogramTableInJFrame {
     return appFrame;
   }
 
-  private Box createNonogramCellsGrid(String[][] nonogramGrid, boolean isBlackAndWhite) {
+private Box createNonogramCellsGridAndCounts(String[][] nonogramGrid, boolean isBlackAndWhite) {
     Box verticalBoxforButtonRows = Box.createVerticalBox();
     NonogramCountsKeysUI nonogramCounsKeys = new NonogramCountsKeysUI();
+    
     if (isBlackAndWhite) {
-      verticalBoxforButtonRows.add(
-          nonogramCounsKeys.addNonogramAllColumnsCounts(new BWCellCounts(nonogramGrid).getAllColumnsBlackCellCount()));
+        verticalBoxforButtonRows.add(createBlackAndWhiteGrid(nonogramGrid, nonogramCounsKeys));
     } else {
-      RGBCellCounts rgbCellCounts = new RGBCellCounts(nonogramGrid);
-
-      Box horisontalColorsOfColumnCounts = nonogramCounsKeys
-          .addNonogramAllColumnsColorsOfCounts(rgbCellCounts.getColorsOfCellCountsColumns());
-      horisontalColorsOfColumnCounts.add(createButtonOverVerticalCounts());
-      verticalBoxforButtonRows.add(horisontalColorsOfColumnCounts);
-
-      Box horisontalColumCounts = nonogramCounsKeys
-          .addNonogramAllColumnsCounts(rgbCellCounts.getColorCellCountsColumns());
-      horisontalColumCounts.add(createButtonOverVerticalCounts());
-      verticalBoxforButtonRows.add(horisontalColumCounts);
+        verticalBoxforButtonRows.add(createColorGrid(nonogramGrid, nonogramCounsKeys));
     }
 
     for (int row = 0; row < nonogramGrid.length; row++) {
-      Box buttonRowBox = getOneNonogramRow(nonogramGrid[row]);
-      if (isBlackAndWhite) {
-        nonogramCounsKeys.addNonogramOneRowCounts(new BWCellCounts(nonogramGrid).getAllRowsBlackCellCounts().get(row),
-            buttonRowBox);
-      } else {
-        RGBCellCounts rgbCellCounts = new RGBCellCounts(nonogramGrid);
-        nonogramCounsKeys.addNonogramOneRowCounts(rgbCellCounts.getColorCellCountsRows().get(row), buttonRowBox);
-        nonogramCounsKeys.addNonogramOneRowColoursOfCounts(rgbCellCounts.getColorsOfCellCountsRows().get(row),
-            buttonRowBox);
-      }
-      verticalBoxforButtonRows.add(buttonRowBox);
+        Box buttonRowBox = getOneNonogramRow(nonogramGrid[row]);
+        if (isBlackAndWhite) {
+            nonogramCounsKeys.addNonogramOneRowCounts(new BWCellCounts(nonogramGrid).getAllRowsBlackCellCounts().get(row),
+                buttonRowBox);
+        } else {
+            RGBCellCounts rgbCellCounts = new RGBCellCounts(nonogramGrid);
+            nonogramCounsKeys.addNonogramOneRowCounts(rgbCellCounts.getColorCellCountsRows().get(row), buttonRowBox);
+            nonogramCounsKeys.addNonogramOneRowColoursOfCounts(rgbCellCounts.getColorsOfCellCountsRows().get(row),
+                buttonRowBox);
+        }
+        verticalBoxforButtonRows.add(buttonRowBox);
     }
     return verticalBoxforButtonRows;
-  }
+}
+
+private Box createBlackAndWhiteGrid(String[][] nonogramGrid, NonogramCountsKeysUI nonogramCountsKeysUI) {
+    return nonogramCountsKeysUI.addNonogramAllColumnsCounts(new BWCellCounts(nonogramGrid).getAllColumnsBlackCellCount());
+}
+
+private Box createColorGrid(String[][] nonogramGrid, NonogramCountsKeysUI nonogramCountsKeysUI) {
+    RGBCellCounts rgbCellCounts = new RGBCellCounts(nonogramGrid);
+
+    Box horizontalColorsOfColumnCounts = nonogramCountsKeysUI
+            .addNonogramAllColumnsColorsOfCounts(rgbCellCounts.getColorsOfCellCountsColumns());
+    horizontalColorsOfColumnCounts.add(createButtonOverVerticalCounts());
+
+    Box horizontalColumnCounts = nonogramCountsKeysUI
+            .addNonogramAllColumnsCounts(rgbCellCounts.getColorCellCountsColumns());
+    horizontalColumnCounts.add(createButtonOverVerticalCounts());
+
+    Box verticalBox = Box.createVerticalBox();
+    verticalBox.add(horizontalColorsOfColumnCounts);
+    verticalBox.add(horizontalColumnCounts);
+    return verticalBox;
+}
+
 
   private JButton createButtonOverVerticalCounts() {
     JButton buttonOverVerticalCounts = new JButton();
